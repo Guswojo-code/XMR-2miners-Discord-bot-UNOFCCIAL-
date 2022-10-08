68f28cb8315c8aa7316aa7a03347580dc919ae74
Guswojo-code <74015971+Guswojo-code@users.noreply.github.com>
2022-10-08 20:52:51 +0200

    Added MH/s, KH/s and H/s
    Added MH/s, KH/s and H/s

--- main.py
+++ main.py
@@ -53,9 +53,36 @@ async def info(ctx, args1):
     workers = data["workersTotal"]
     #print(json.dumps(data, indent=4))
     
+    #Math
+    if current_hashrate > 1000000:
+        current_units = " MH/s"
+        current_hashrate = current_hashrate/1000000
+
+    elif current_hashrate > 1000:
+        current_units = " KH/s"
+        current_hashrate = current_hashrate/1000
+    else:
+        current_units = " H/s"
+
+    if avr_hashrate > 1000000:
+        avr_units = " MH/s"
+        avr_hashrate = avr_hashrate/1000000
+
+    elif avr_hashrate > 1000:
+        avr_units = " KH/s"
+        avr_hashrate = avr_hashrate/1000
+    else:
+        avr_units = " H/s"
+   
+    #Format the numbers so they look *Beautiful*
+
+    current_hashrate_formatted = "{:.2f}".format(current_hashrate)
+    avr_hashrate_formatted = "{:.2f}".format(avr_hashrate)
+
+
     #Send the results:
     await ctx.message.delete()
-    embed=discord.Embed(title="Current hashrate: " + str(current_hashrate) + "\n Average hashrate: " + str(avr_hashrate) + "\n Workers online: " + str(workersOnline) + " / " + str(workers), color=0xFF5733)
+    embed = discord.Embed(title="Current hashrate: " + str(current_hashrate_formatted) + current_units + "\n Average hashrate: " + str(avr_hashrate_formatted) + avr_units + "\n Workers online: " + str(workersOnline) + " / " + str(workers), color=0xFF5733)
     embed.set_thumbnail(url='https://www.getmonero.org/press-kit/symbols/monero-symbol-on-white-480.png')
     embed.timestamp = datetime.datetime.utcnow()
     await ctx.send(embed=embed)
